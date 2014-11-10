64da86b6953d32303937f26e7e9d41b75360dd51
diamond-darrell <staskgt@gmail.com>
2014-11-10 13:01:29 +0200

    10

--- core/upload.php
+++ core/upload.php
@@ -3,11 +3,18 @@
 if (!empty($_FILES)) {
 
     $tempPath = $_FILES['file']['tmp_name'];
+
+    // for localhost
     $uploadPath = $_SERVER['DOCUMENT_ROOT'] .
         DIRECTORY_SEPARATOR . 'index' .
         DIRECTORY_SEPARATOR . 'rsslab' .
         DIRECTORY_SEPARATOR . 'images' .
         DIRECTORY_SEPARATOR . $_FILES['file']['name'];
+
+    // for remote host
+//    $uploadPath = $_SERVER['DOCUMENT_ROOT'] .
+//        DIRECTORY_SEPARATOR . 'images' .
+//        DIRECTORY_SEPARATOR . $_FILES['file']['name'];
 
     move_uploaded_file($tempPath, $uploadPath);
 
